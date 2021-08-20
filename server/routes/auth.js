31eb99b4876822e9d875544ed44d485a86a6903a
Kartik <kartikth40@gmail.com>
2021-08-20 21:52:41 +0530

    made specific folder for controllers of every get request.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,9 @@
 const express = require('express')
-
 const router = express.Router()
 
-router.get('/create-or-update-user', (req, res) => {
-  res.json({
-    data: 'hey you hit create-or-update-user Api endpoint',
-  })
-})
+//import
+const { createOrUpdateUser } = require('../controllers/auth')
+
+router.get('/create-or-update-user', createOrUpdateUser)
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -2,11 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const cors = require('cors')
-
+const fs = require('fs')
 require('dotenv').config()
-
-//import routes
-const authRoutes = require('./routes/auth')
 
 //db
 mongoose
@@ -28,7 +25,10 @@ app.use(express.json({ limit: '2mb' }))
 app.use(cors())
 
 //routes middlewares
-app.use('/api', authRoutes)
+// example - importing auth file from routes. The same will be done to all the files in routes by fs module
+// const authRoutes = require('./routes/auth')
+// app.use('/api', authRoutes)
+fs.readdirSync('./routes').map((r) => app.use('/api', require('./routes/' + r)))
 
 //port
 const PORT = process.env.PORT || 8000
