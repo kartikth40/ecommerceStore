6112a24b360b579fe6f07c034d7fd8c079a3b0ec
Kartik <kartikth40@gmail.com>
2021-08-21 21:33:40 +0530

    added firebase service account code and key

---
+++ server/firebase/index.js
@@ -0,0 +1,7 @@
+const admin = require('firebase-admin')
+
+const serviceAccount = require('..config/fbServiceAccountKey.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+})
