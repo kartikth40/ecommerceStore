7e492c5795c4f364c4f54db3476ffc6b4a3190b0
Kartik <kartikth40@gmail.com>
2021-08-22 16:00:34 +0530

    updated the authCheck and createOrUpdateUser functions for the auth route

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,20 @@
-exports.createOrUpdateUser = (req, res) => {
-  res.json({
-    data: 'hey you hit create-or-update-user Api endpoint',
-  })
+const User = require('../models/user')
+
+exports.createOrUpdateUser = async (req, res) => {
+  const { name, picture, email } = req.user
+  const user = await User.findOneAndUpdate(
+    { email: email },
+    { name: name, picture: picture },
+    { new: true }
+  )
+  if (user) {
+    res.json(user)
+  } else {
+    const newUser = await new User({
+      email,
+      name,
+      picture,
+    }).save()
+    res.json(newUser)
+  }
 }
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,6 +1,16 @@
 const admin = require('../firebase')
 
-exports.authCheck = (req, res, callback) => {
-  console.log(req.headers) //token
-  callback()
+exports.authCheck = async (req, res, callback) => {
+  // console.log(req.headers) //token
+  try {
+    const firebaseUser = await admin.auth().verifyIdToken(req.headers.authtoken)
+    console.log('FIREBASE USER IN AUTHCHECK', firebaseUser)
+    req.user = firebaseUser
+    callback()
+  } catch (err) {
+    console.log(err)
+    res.status(401).json({
+      err: 'Invalid or expired Token',
+    })
+  }
 }
