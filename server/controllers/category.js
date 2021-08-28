2592a280cc2bd1dd4d82ed5248f57b6987231369
Kartik <kartikth40@gmail.com>
2021-08-28 16:53:43 +0530

    created a new mongodb model to save the categories and made routes for the same in backend.

---
+++ server/controllers/category.js
@@ -0,0 +1,49 @@
+const Category = require('../models/category')
+const slugify = require('slugify')
+
+exports.create = async (req, res) => {
+  try {
+    const { name } = req.body
+    const category = await new Category({
+      name: name,
+      slug: slugify(name),
+    }).save()
+    res.json(category)
+  } catch (err) {
+    res.status(400).send('Create category failed.')
+  }
+}
+
+exports.list = async (req, res) => {
+  list = await Category.find({}).sort({ createdAt: -1 }).exec()
+  res.json(list)
+}
+
+exports.read = async (req, res) => {
+  let category = await Category.findOne({ slug: req.params.slug }).exec()
+  res.json(category)
+}
+
+exports.update = async (req, res) => {
+  const { name } = req.body
+  try {
+    const updated = await Category.findOneAndUpdate(
+      { slug: req.params.slug },
+      { name, slug: slugify(name) },
+      { new: true }
+    )
+    res.json(updated)
+  } catch (err) {
+    res.status(400).send('Category update failed')
+  }
+}
+
+exports.remove = async (req, res) => {
+  try {
+    const deleted = await Category.findOneAndDelete({ slug: req.params.slug })
+    res.json(deleted)
+  } catch (err) {
+    console.log(err)
+    res.status(400).send('Category delete failed')
+  }
+}
---
+++ server/models/category.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      minlength: [2, 'Too short'],
+      maxlength: [32, 'Too long'],
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Category', categorySchema)
