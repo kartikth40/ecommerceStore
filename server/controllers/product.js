86bdea79cbe9f40cf074a3dfb7edc1c650786fd5
Kartik <kartikth40@gmail.com>
2021-10-18 14:18:06 +0530

    made the products controller to read all the products from the database

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -15,7 +15,12 @@ exports.create = async (req, res) => {
   }
 }
 
-exports.read = async (req, res) => {
+exports.listAll = async (req, res) => {
   let products = await Product.find({})
+    .limit(parseInt(req.params.count))
+    .populate('category')
+    .populate('subs')
+    .sort([['createdAt', 'desc']])
+    .exec()
   res.json(products)
 }
--- server/routes/product.js
+++ server/routes/product.js
@@ -5,9 +5,9 @@ const router = express.Router()
 const { authCheck, adminCheck } = require('../middlewares/auth')
 
 //controller
-const { create, read } = require('../controllers/product')
+const { create, listAll } = require('../controllers/product')
 
 router.post('/product', authCheck, adminCheck, create) // (getURL, middleware, controller)
-router.get('/products', read)
+router.get('/products/:count', listAll)
 
 module.exports = router
