f54bfc095a9118dfcfe690203af7f08e92256757
Kartik <kartikth40@gmail.com>
2022-01-23 15:29:05 +0530

    made an end point in backend to get the cart details in checkout page from database only because user could manipulate the details in frontend so we cant trust frontend here

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -46,3 +46,15 @@ exports.userCart = async (req, res) => {
 
   res.json({ ok: true })
 }
+
+exports.getUserCart = async (req, res) => {
+  const user = await User.findOne({ email: req.user.email }).exec()
+
+  let cart = await Cart.findOne({ orderedBy: user._id })
+    .populate('products.product', '_id title price totalAfterDiscount')
+    .exec()
+
+  const { products, cartTotal, totalAfterDiscount } = cart
+
+  res.json({ products, cartTotal, totalAfterDiscount })
+}
