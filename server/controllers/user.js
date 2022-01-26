be3518bb4197b77b5c96a8c7855034209a3b72b3
Kartik <kartikth40@gmail.com>
2022-01-26 23:32:25 +0530

    made backend endpoints to create new order after successful purchase

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,6 +2,7 @@ const User = require('../models/user')
 const Product = require('../models/product')
 const Cart = require('../models/cart')
 const Coupon = require('../models/coupon')
+const Order = require('../models/order')
 
 exports.userCart = async (req, res) => {
   const { cart } = req.body
@@ -104,4 +105,19 @@ exports.applyCouponToUserCart = async (req, res) => {
   ).exec()
 
   res.json({ totalAfterDiscount, discount })
+}
+
+exports.createOrder = async (req, res) => {
+  const { paymentIntent } = req.body.stripeResponse
+  const user = await User.findOne({ email: req.user.email }).exec()
+
+  let { products } = await Cart.findOne({ orderedBy: user._id }).exec()
+
+  let newOrder = await new Order({
+    products,
+    paymentIntent,
+    orderedBy: user._id,
+  }).save()
+
+  res.json({ ok: true })
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -11,12 +11,16 @@ const {
   emptyCart,
   saveAddress,
   applyCouponToUserCart,
+  createOrder,
 } = require('../controllers/user')
 
 router.post('/user/cart', authCheck, userCart) // save cart
 router.get('/user/cart', authCheck, getUserCart) // get cart
 router.delete('/user/cart', authCheck, emptyCart) // empty cart
 router.post('/user/address', authCheck, saveAddress) // empty cart
+
 router.post('/user/cart/coupon', authCheck, applyCouponToUserCart) // apply coupon
+
+router.post('/user/order', authCheck, createOrder) // create order after successful purchase
 
 module.exports = router
